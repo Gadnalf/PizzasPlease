6d4ce23d4f72deca4ef2c0f5ccefadcb99446532
Joshua Lenander <42joshl@gmail.com>
2023-07-09 11:50:25 -0400

    Add Bell ding sound to title screen start

--- Assets/Scripts/NewScene.cs
+++ Assets/Scripts/NewScene.cs
@@ -5,7 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class NewScene : MonoBehaviour
 {
+    public float delay = 1.0f;
+
     public void NextScene() {
         SceneManager.LoadScene("LoadingScene");
+    }
+
+    public void NextSceneWithDelay()
+    {
+        Invoke("NextScene", delay);
     }
 }
