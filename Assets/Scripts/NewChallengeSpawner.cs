6a35fd60445ab2571a28e01519fa10437756d741
Jimbo Xie <jimboxie@gmail.com>
2023-07-08 21:59:33 -0400

    Score and Streak system

--- Assets/Scripts/NewChallengeSpawner.cs
+++ Assets/Scripts/NewChallengeSpawner.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class NewChallengeSpawner : MonoBehaviour
 {
     public GameObject instantiatedPizza;
     public GameObject instantiatedReceipt;
     public GameObject alert;
+    public GameObject scoreText;
+    public GameObject streakText;
 
     private PizzaFactory pizzaFactory;
     private int warningPoints;
+    public static int score;
+    private int streak;
     public bool currentPizzaGood;
     void Start() {
         pizzaFactory = GetComponent<PizzaFactory>();
@@ -20,24 +26,42 @@ public class NewChallengeSpawner : MonoBehaviour
         }
     }
 
-    public void onGoodReview() {
-        if (!currentPizzaGood) {
+    public void Review(bool incorrect){
+        if (incorrect) {
+            streak = 0;
+            score -= 500;
             StartCoroutine(ErroredJudgement());
+            streakText.GetComponent<TextMeshProUGUI>().faceColor = Color.red;
+            streakText.GetComponent<TextMeshProUGUI>().text = "- 500";
+        } else {
+            streak = Math.Min(streak + 1, 3);
+            score += 100*streak;
+            streakText.GetComponent<TextMeshProUGUI>().faceColor = Color.green;
+            streakText.GetComponent<TextMeshProUGUI>().text = "+ " + (streak*100).ToString();
         }
+        scoreText.GetComponent<TextMeshProUGUI>().text = "Score: " + score.ToString();
+        StartCoroutine(ShowStreak());
         OnReviewSubmitted();
     }
 
+    public void onGoodReview() {
+        Review(!currentPizzaGood);
+    }
+
     public void onBadReview() {
-        if (currentPizzaGood) {
-            StartCoroutine(ErroredJudgement());
-        }
-        OnReviewSubmitted();
+        Review(currentPizzaGood);
     }
 
     public void OnReviewSubmitted() {
         Destroy(instantiatedPizza);
         Destroy(instantiatedReceipt);
         OrderNewPizza();
+    }
+
+    IEnumerator ShowStreak(){
+        streakText.GetComponent<TextMeshProUGUI>().enabled = true;
+        yield return new WaitForSeconds(2f);
+        streakText.GetComponent<TextMeshProUGUI>().enabled = false;
     }
 
     IEnumerator ErroredJudgement() {
