95f4804b1ec90aeed825e8f15cc14f8bbe0e946f
Philip <philip.cai.mao@hotmail.ca>
2023-07-08 21:39:43 -0400

    move script

---
+++ Assets/Scripts/ZelpController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZelpController : MonoBehaviour
+{
+    public struct Review
+    {
+        public Review(int stars, string text)
+        {
+            Stars = stars;
+            TextContent = text;
+        }
+
+        public int Stars { get; set; }
+        public string TextContent { get; set; }
+    }
+
+    Queue<Review> lastNReviews = new Queue<Review>();
+}
